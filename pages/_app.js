734c03f4ad2c754f3bb95925a363841ce3e320c1
AnkurMaurya421 <ankurmaurya421@gmail.com>
2026-03-07 12:12:48 +0530

    update app.js to wrap the all pages with Checkout Provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,11 @@
 import "@/styles/globals.css";
 import '../styles/checkout.css';
+import { CheckoutProvider } from '../context/CheckoutContext';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <CheckoutProvider>
+      <Component {...pageProps} />
+    </CheckoutProvider>
+  );
 }
